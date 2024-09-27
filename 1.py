545bafce79606cf5241ec4c42a124c21d8d32bc1
sameeracharya11 <sameeracharya419@gmail.com>
2024-09-27 13:10:29 +0530

    calculator

---
+++ 1.py
@@ -0,0 +1,8 @@
+num1=8
+num2=4
+sum1=num1+num2
+print(sum1)
+num3=55
+num4=65
+sum2=num3+num4
+print(sum2)
---
+++ 3.py
@@ -0,0 +1,3 @@
+num1=input('enter first number')
+num2=input('enter second number')
+ calc1(num1,num2)
